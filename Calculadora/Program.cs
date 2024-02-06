0f43ca42ad904ac9fa00ac8ea1f31de829b3b6c8
PedroLuc2s <mrunvisauusr@gmail.com>
2024-02-06 12:12:38 -0300

    SW/Cal

---
+++ Calculadora/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading;
+
+namespace Calculator
+{
+    class Program
+    {
+        static float Multiply(float a, float b) { return a * b; }
+
+        static float Divide(float a, float b) { return a / b; }
+
+        static float Subtract(float a, float b) { return a - b; }
+
+        static float Add(float a, float b) { return a + b; }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("Primeiro valor: ");
+            var primeiraVariavel = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor: ");
+            var segundaVariavel = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Escolha a operação: (+, -, *, /)");
+            char operation = char.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            switch (operation)
+            {
+                case '+': Console.WriteLine($"O resultado da operação é: {Add(primeiraVariavel, segundaVariavel)}"); break;
+                case '-': Console.WriteLine($"O resultado da operação é: {Subtract(primeiraVariavel, segundaVariavel)}"); break;
+                case '*': Console.WriteLine($"O resultado da operação é: {Multiply(primeiraVariavel, segundaVariavel)}"); break;
+                case '/': Console.WriteLine($"O resultado da operação é: {Divide(primeiraVariavel, segundaVariavel)}"); break;
+                default: Console.WriteLine("Operação inválida!!"); break;
+            }
+            
+            Thread.Sleep(2000);
+            Console.WriteLine("");
+            Console.WriteLine("Para voltar ao menu digite 'c'");
+            Thread.Sleep(2000);
+            Console.WriteLine("Caso queira sair Digite a tecla 's'");
+            Console.WriteLine("");
+            Thread.Sleep(1000);
+            string tecla = Console.ReadLine().ToLower();
+
+            if (tecla == "c")
+                Menu();
+
+            if (tecla == "s")
+                Environment.Exit(0);
+        }
+
+        static void Main()
+        {
+            Menu();
+        }
+    }
+}
---
+++ Stopwatch/Program.cs
@@ -0,0 +1,62 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+
+using System;
+using System.Threading;
+
+namespace StopWatch
+{
+    class Program
+    {
+        static void Main()
+        {
+            Menu();
+        }
+        static void PreStart()
+        {
+            Console.Clear();
+            Console.WriteLine("Ready...");
+            Thread.Sleep(1500);
+            Console.WriteLine("Set...");
+            Thread.Sleep(1500);
+            Console.WriteLine("Go...");
+            Thread.Sleep(500);
+        }
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("Digite o tempo em segundos ou digite 'exit' para sair: ");
+            #pragma warning disable CS8602 // Desreferência de uma referência possivelmente nula.
+            var data = Console.ReadLine().ToLower();
+            #pragma warning restore CS8602 // Desreferência de uma referência possivelmente nula.
+            if (data == "exit")
+                System.Environment.Exit(0);
+            else
+            {
+                int time = int.Parse(data);
+                PreStart();
+                Start(time);
+            }
+        }
+        static void Start(int time)
+        {
+            int currenTitme = 0;
+
+            while (currenTitme != time)
+            {
+                Console.Clear();
+                currenTitme++;
+                Console.WriteLine($"{currenTitme}s");
+                Thread.Sleep(1000);            
+            }
+            Console.Clear();
+            Console.WriteLine("O StopWatch terminou... Pressione 'Enter' para prosseguir.");
+            Thread.Sleep(2000);
+            Console.ReadKey();
+            Menu();            
+        }
+    }
+}
+
+
